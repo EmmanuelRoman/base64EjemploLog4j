a35756dd3bc054a1366f215ae420b90e0e5d0ed9
Emmanuel Roman Santibanez <romane@mx.aon.bz>
2018-09-26 11:56:20 -0500

    Uso de Stringbuilder para el tratamiento en Base64

--- src/main/java/com/base64/ejemplo/app/EjemploBase64.java
+++ src/main/java/com/base64/ejemplo/app/EjemploBase64.java
@@ -12,13 +12,20 @@ public class EjemploBase64 {
 
 	public static void ejemploBase64Java() {
 		String originalInput = "Emmanuel Román Santibáñez";
-
+		StringBuilder input = new StringBuilder("base64_");
+		
 		// Encode data on your side using BASE64
 		String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
 		logger.info("java.util.Base64 encoded value is " + encodedString);
-
+		logger.info("java.util.Base64 len value is " + encodedString.length());
+		
+		input.append(encodedString);
+		logger.info("StringBuilder encoded append value is " + input.toString());
+		logger.info("StringBuilder encoded len value is " + input.toString().length());
+		logger.info("StringBuilder encoded substring value is " + input.substring(7));
+		
 		// Decode data on other side, by processing encoded data
-		byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+		byte[] decodedBytes = Base64.getDecoder().decode(input.substring(7));
 		String decodedString = new String(decodedBytes);
 		logger.info("java.util.Base64 Decoded value is " + decodedString);
 	}
